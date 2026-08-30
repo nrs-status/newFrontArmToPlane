1e57e3053304986a025d92e36b7632c280efe8fd
nrs-status <sebaarsim@gmail.com>
2026-08-30 04:31:29 +0000

    added default fix config files

--- templeArtemisEphesus/fishConfig/default.nix
+++ templeArtemisEphesus/fishConfig/default.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }:
+#currently, everything except `./fishConfig.nix` and its imports are part of the default fish config
 pkgs.writeTextFile {
   name = "fish config";
   text = import ./fishConfig.nix;
