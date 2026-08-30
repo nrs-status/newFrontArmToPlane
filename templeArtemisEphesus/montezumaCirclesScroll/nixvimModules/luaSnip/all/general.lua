7f935ca777151710d4ce41e1c62ff4de95b82bd4
nrs-status <sebaarsim@gmail.com>
2026-08-30 01:12:16 +0000

    fixing luasnip

--- templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/all/general.lua
+++ templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/all/general.lua
@@ -10,11 +10,18 @@ return {
     {
       trig = "agentBasic",
       name = "agent basic prompt",
-    },
-    {
-      t({ "This is the top", "" }),
-      i(1),
-      t({ "", "This is the bottom" }),
-    }
+    }, fmt(
+      [[ 
+      You are on a NixOS system. If you need tools, write a flake.nix file and run a shell from it.
+
+      {}
+
+Once you are done: 
+- Provide a step-by-step summary of all steps you have undertaken.
+- Create a file called SIGNATURE.md containing the current date, the name of the model that ran these instructions, the total API token usage and the total total API costs. In order to obtain the API costs, rely on the pi coding agent's report about the current session.
+
+      ]],
+      { i(1) },
+    )
   ),
 }
