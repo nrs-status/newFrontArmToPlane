069cd2efdcd55f03dfe4fa921a2d85a68242df71
nrs-status <sebaarsim@gmail.com>
2026-08-30 00:42:46 +0000

    fixing luasnip

--- templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/default.nix
+++ templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/default.nix
@@ -7,4 +7,19 @@
       ; }
     ];
   };
+
+  keymaps = [
+    {
+      mode = "i";
+      key = "<C-k>";
+      action = "<cmd>lua require('luasnip').expand_or_jump()<cr>";
+      options.desc = "LuaSnip: expand snippet or jump to next node";
+    }
+    {
+      mode = [ "i" "s" ];
+      key = "<C-j>";
+      action = "<cmd>lua require('luasnip').jump(-1)<cr>";
+      options.desc = "LuaSnip: jump to previous node";
+    }
+  ];
 }
