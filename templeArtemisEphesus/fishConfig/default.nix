3dcb23cfe2f5863585e9d51c6de291c679054da3
nrs-status <sebaarsim@gmail.com>
2026-08-30 04:41:40 +0000

    fixing packaging for fish config

--- templeArtemisEphesus/fishConfig/default.nix
+++ templeArtemisEphesus/fishConfig/default.nix
@@ -1,8 +1,5 @@
 { pkgs, ... }:
-let fishConfigProper = pkgs.writeTextFile {
-  name = "fishConfigMainFile";
-  text = import ./fishConfig.nix;
-}; in pkgs.stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation {
     name = "fishConfig";
     src = ./fishDefaultConfigFiles;
     installPhase = ''
@@ -10,7 +7,7 @@ let fishConfigProper = pkgs.writeTextFile {
 
       cp -r $src/* $out
 
-      install -Dm644 ${pkgs.writeText "config.fish" fishConfigProper} $out
+      install -Dm644 ${pkgs.writeText "config.fish" (import ./fishConfig.nix)} $out
 
       runHook postInstall
       '';
