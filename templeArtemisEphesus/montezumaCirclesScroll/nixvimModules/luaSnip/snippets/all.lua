4901976673dfc50bf8e7232e3f752c7c3446f712
nrs-status <sebaarsim@gmail.com>
2026-08-30 00:21:42 +0000

    fixing luasnip

--- templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/snippets/all.lua
+++ templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/snippets/all.lua
@@ -1,5 +1,5 @@
 return {
-  require("luasnip").s( { trig = "foo" }, { require("luasnip").t("hello world!")} ),
+  require("luasnip").s( { trig = "foo", desc = "my snippet" }, { require("luasnip").t("hello world!")} ),
   require("luasnip").s(
     { trig = "example", desc = "Snippet with fixed top, user input middle, fixed bottom" },
     require("luasnip").sn(nil, {
