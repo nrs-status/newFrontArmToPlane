d80d241e0f8aaf9e58b8279d55c475d03a529429
nrs-status <sebaarsim@gmail.com>
2026-08-30 22:22:45 +0000

    added secrets and a method of decryption

--- templeArtemisEphesus/secrets/default.nix
+++ templeArtemisEphesus/secrets/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 pkgs.linkFarm "secrets" [
-  { name = ".sops.yaml"; path = pkgs.writeTextFile "sops-yaml-file" (builtins.readFile ./.sops.yaml); }
+  { name = ".sops.yaml"; path = pkgs.writeText "sops-yaml-file" (builtins.readFile ./.sops.yaml); }
   { name = "secrets.yaml"; path = pkgs.writeText "secrets-yaml-file" (builtins.readFile ./secrets.yaml); }
 ]
 # pkgs.symlinkJoin {
