7b0d8d68d774b701aaea99d2d3b4cd6e9c7e68d0
nrs-status <sebaarsim@gmail.com>
2026-08-30 00:44:54 +0000

    fixing luasnip

--- templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/all/general.lua
+++ templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/all/general.lua
@@ -1,6 +1,6 @@
 return {
   require("luasnip").s(
-    { trig = "example", desc = "Snippet with fixed top, user input middle, fixed bottom" },
+    { trig = "mytesting", name = "testing", desc = "Snippet with fixed top, user input middle, fixed bottom" },
     require("luasnip").sn(nil, {
       require("luasnip").t({ "Top of the lua snippet", "" }),
       require("luasnip").i(1),
--- templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/default.nix
+++ templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/luaSnip/default.nix
@@ -2,9 +2,9 @@
   plugins.luasnip = {
     enable = true;
     fromLua = [
-      { paths = 
-        ./.
-      ; }
+      {
+        paths = ./.;
+      }
     ];
   };
 
@@ -16,7 +16,10 @@
       options.desc = "LuaSnip: expand snippet or jump to next node";
     }
     {
-      mode = [ "i" "s" ];
+      mode = [
+        "i"
+        "s"
+      ];
       key = "<C-j>";
       action = "<cmd>lua require('luasnip').jump(-1)<cr>";
       options.desc = "LuaSnip: jump to previous node";
