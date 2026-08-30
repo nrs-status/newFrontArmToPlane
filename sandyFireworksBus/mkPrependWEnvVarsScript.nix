0fbcb50a4efcba43e7850beeea8e348a607101d2
nrs-status <sebaarsim@gmail.com>
2026-08-30 22:16:19 +0000

    added secrets and a method of decryption

--- sandyFireworksBus/mkPrependWEnvVarsScript.nix
+++ sandyFireworksBus/mkPrependWEnvVarsScript.nix
@@ -1,13 +1,19 @@
 { pkgs, pkgsLib, ... }:
-{ scriptName, packageToWrap, envVars, runtimeInputs ? [] }:
-#pkgs.writeShellApplication 
 {
+  scriptName,
+  packageToWrap,
+  envVars,
+  runtimeInputs ? [ ],
+}:
+pkgs.writeShellApplication {
   name = scriptName;
   inherit runtimeInputs;
   text = ''
-  ${pkgsLib.concatStringsSep "\n" (
-    pkgsLib.mapAttrsToList (name: value: "export ${name}=${pkgsLib.escapeShellArg (toString value)}") envVars
-  )}
-  exec ${pkgsLib.getExe packageToWrap} "$@"
+    ${pkgsLib.concatStringsSep "\n" (
+      pkgsLib.mapAttrsToList (
+        name: value: "export ${name}=${pkgsLib.escapeShellArg (toString value)}"
+      ) envVars
+    )}
+    exec ${pkgsLib.getExe packageToWrap} "$@"
   '';
 }
