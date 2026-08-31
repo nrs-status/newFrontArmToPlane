a2775641a5e4e406ef794536af7f7e12a164d0dc
nrs-status <sebaarsim@gmail.com>
2026-08-31 04:51:47 +0000

    fixing pi

--- templeArtemisEphesus/piWConfig/default.nix
+++ templeArtemisEphesus/piWConfig/default.nix
@@ -6,7 +6,7 @@
   ...
 }:
 let
-  bashInterpolationToGetAPIKey = "\"$(SOPS_AGE_KEY=$(${pkgsLib.getExe keyReader}) ${pkgsLib.getExe localPkgs.scripts.decryptSecret} ${localPkgs.secrets}/secrets.yaml OPENROUTER_API_KEY)\"";
+  bashInterpolationToGetAPIKey = "\"! $(SOPS_AGE_KEY=$(${pkgsLib.getExe keyReader}) ${pkgsLib.getExe localPkgs.scripts.decryptSecret} ${localPkgs.secrets}/secrets.yaml OPENROUTER_API_KEY)\"";
   keyReader = localLib.mkKeyReader {
     envVarName = "SOPS_AGE_KEY";
     keyPath = "/etc/keys.yaml";
@@ -27,7 +27,7 @@ in
 
       makeWrapper ${pkgsLib.getExe pkgs.pi-coding-agent} $out/bin/pi \
         --run 'mkdir -p ~/.config/pi/agent' \
-        --run 'cat ${jsonAuthFileContent} > ~/.config/pi/agent/auth.json'
+        --run "echo \"${jsonAuthFileContent}\" > ~/.config/pi/agent/auth.json"
 
       runHook postInstall
       '';
