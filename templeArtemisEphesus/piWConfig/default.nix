a324e05f8c7f4f6a16431b6ab9c905b7c695f211
nrs-status <sebaarsim@gmail.com>
2026-08-31 04:05:57 +0000

    updating lockfile and testing out a fix for pi

--- templeArtemisEphesus/piWConfig/default.nix
+++ templeArtemisEphesus/piWConfig/default.nix
@@ -28,7 +28,6 @@ let
       cp $src/* $out
 
       install -Dm644 ${pkgs.writeText "pi-auth-json-file" jsonAuthFileContent} $out/auth.json
-      mkdir -p $out/auth.json.lock
 
       runHook postInstall
     '';
