89c3f470a51b130f686deec4a115da7e2454f927
nrs-status <sebaarsim@gmail.com>
2026-08-31 02:26:18 +0000

    added a combination of the previous env var wrapper and opt feeder scripts

---
+++ sandyFireworksBus/mkOptAndEnvWrapperScript.nix
@@ -0,0 +1,36 @@
+{ pkgs, pkgsLib, ... }:
+{
+  name,
+  pkgToWrap,
+  opts,
+  envVars,
+  runtimeInputs,
+}:
+let
+  renderOpt =
+    {
+      dash,
+      optName,
+      val ? null,
+    }:
+    let
+      flag = "${dash}${optName}";
+    in
+    assert pkgsLib.assertMsg (
+      dash == "-" || dash == "--"
+    ) "mkOptionsFeederScript: the argument `dash` must be either \"-\" or \"--\"";
+    if val == null then flag else flag + " " + val;
+  renderedOpts = pkgsLib.concatMapStringSep " " renderOpt opts;
+  renderedEnvVarDecls = pkgsLib.concatStringsSep "\n" (
+    pkgsLib.concatMap (entry: [
+      "${entry.key}=${entry.value}"
+      "export ${entry.key}"
+    ]) envVars
+  );
+in
+pkgs.writeShellApplication {
+  inherit name runtimeInputs;
+  text = ''
+    ${renderedEnvVarDecls}
+        exec ${pkgsLib.getExe pkgToWrap} ${renderedOpts} "$@"'';
+}
