40a2e547cc5d7eb680abc0fbc663dea739f0c328
nrs-status <sebaarsim@gmail.com>
2026-08-31 01:34:15 +0000

    instead of feeding the api keys to `pi` through an env vars, feeding it through a config. otherwise the agents have access to the key

---
+++ templeArtemisEphesus/scripts/piWConfig.nix
@@ -0,0 +1,36 @@
+{ pkgs, localLib, pkgsLib, localPkgs, ... }:
+let
+  keyReader = localLib.mkKeyReader {
+    envVarName = "SOPS_AGE_KEY";
+    keyPath = "/etc/keys.yaml";
+  };
+  authJsonFileContents = builtins.toJSON {
+    openrouter = {
+      type = "api_key";
+      key = "!$(SOPS_AGE_KEY=$(${pkgsLib.getExe keyReader}) ${pkgsLib.getExe localPkgs.scripts.decryptSecret} ${localPkgs.secrets}/secrets.yaml OPENROUTER_API_KEY)";
+    };
+  };
+  configDirDrv = pkgs.stdenv.mkDerivation {
+    name = "piConfigDir";
+    nativeBuildInputs = [  keyReader localPkgs.scripts.decryptSecret localPkgs.secrets ];
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out 
+
+      install -Dm644 ${pkgs.writeText "pi-harness-auth-json" authJsonFileContents} $out/auth.json
+
+      runHook postInstall
+    '';
+  };
+in
+localLib.mkPrependWEnvVarsScript {
+  pkgToWrap = pkgs.pi-coding-agent;
+  envVars = [
+    {
+      key = "PI_CODING_AGENT_DIR";
+      value = configDirDrv;
+    }
+  ];
+
+}
