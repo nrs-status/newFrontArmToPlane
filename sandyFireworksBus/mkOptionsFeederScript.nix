ac8f30756f69c6201bbd78b2f98c2be8a97d833c
nrs-status <sebaarsim@gmail.com>
2026-08-31 01:35:02 +0000

    nix expression to generate a wrapper that wraps a binary executable with options specified by an attribute set

---
+++ sandyFireworksBus/mkOptionsFeederScript.nix
@@ -0,0 +1,27 @@
+{ pkgs, pkgsLib, ... }:
+{
+  pkgToWrap,
+  opts,
+  runtimeInputs,
+}:
+let
+  renderOpt =
+    {
+      dash,
+      optName,
+      val ? null,
+    }:
+    let
+      flag = "${dash}${optName}";
+    in
+    assert pkgsLib.assertMsg (
+      dash == "-" || dash == "--"
+    ) "mkOptionsFeederScript: the argument `dash` must be either \"-\" or \"--\"";
+    if val == null then flag else flag + " " + val;
+  renderedOpts = pkgsLib.concatMapStringSep " " renderOpt opts;
+in
+pkgs.writeShellApplication {
+  name = pkgToWrap.pname or (builtins.parseDrvName pkgToWrap.name).name;
+  inherit runtimeInputs;
+  text = ''exec ${pkgsLib.getExe pkgToWrap} ${renderedOpts} "$@"'';
+}
