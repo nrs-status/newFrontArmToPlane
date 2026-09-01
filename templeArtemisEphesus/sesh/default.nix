43423a818e0dfb65f2d04031738ddfd534b13d40
nrs-status <sebaarsim@gmail.com>
2026-09-01 05:45:55 +0000

    packaged sesh with config

---
+++ templeArtemisEphesus/sesh/default.nix
@@ -0,0 +1,24 @@
+{ pkgs, pkgsLib, ... }:
+pkgs.stdenv.mkDerivation {
+  name = "sesh";
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+  src = ./.;
+  installPhase = ''
+  runHook preInstall
+
+  mkdir -p $out/bin $out/config
+
+  install -Dm644 basic.toml $out/config
+
+  cat > $out/config/main.toml <<EOF
+  import_paths = [
+    "$out/config/basic.toml",
+  ]
+  EOF
+
+  makeWrapper ${pkgsLib.getExe pkgs.sesh} $out/bin/sesh \
+  --prefix PATH : ${pkgsLib.makeBinPath [ pkgs.tmux ]} \
+  --add-flags "--config $out/main.toml"
+
+  runHook postInstall'';
+}
