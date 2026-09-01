0cf9620557ad56ad96b802e4bd6baf182c177b9c
nrs-status <sebaarsim@gmail.com>
2026-09-01 05:23:23 +0000

    added tmux wrapper with config

---
+++ templeArtemisEphesus/tmux/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+pkgs.stdenv.mkDerivation {
+  name = "tmux";
+  src = ./.;
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+  buildInputs = [ pkgs.tmux ];
+  installPhase = ''
+  runHook preInstall
+
+  mkdir -p $out/bin $out/config
+
+  install -Dm644 inheritedConf.conf $out/config
+
+  cat > $out/config/main.conf <<EOF
+    source-file $out/config/inheritedConf.conf
+  EOF
+
+  makeWrapper ${pkgs.tmux}/bin/tmux $out/bin/tmux \
+  --add-flags "-f $out/config/main.conf"
+  
+  runHook postInstall'';
+}
