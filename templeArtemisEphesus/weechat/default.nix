6234af3cac954c3c4778888a29851eaa99afa5b6
nrs-status <sebaarsim@gmail.com>
2026-09-03 21:35:00 +0000

    fixing weechat pkg

--- templeArtemisEphesus/weechat/default.nix
+++ templeArtemisEphesus/weechat/default.nix
@@ -8,7 +8,7 @@ localLib.mkWrapperScript {
   pkgToWrap = pkgs.weechat;
   src = ./.;
   preExecCommands = [
-    "rm -f ~/.config/weechat"
+    "rm -rf ~/.config/weechat"
     "mkdir -p ~/.config/weechat"
     "cp $src/matrix.config $src/irc.config ~/.config/weechat"
   ];
