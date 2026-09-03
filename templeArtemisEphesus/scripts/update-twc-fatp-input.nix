a144164661061dff428e3ae0a7a9b3e8f4cfb383
nrs-status <sebaarsim@gmail.com>
2026-09-03 04:09:36 +0000

    added script to more easily update thatWaterCharmander

---
+++ templeArtemisEphesus/scripts/update-twc-fatp-input.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+pkgs.writeShellApplication {
+  name = "update-twc-fatp-input";
+  text = ''
+    #!/usr/bin/env bash
+
+    set -euo pipefail
+
+    initial_path="$PWD"
+
+    cd "''${THATWATERCHARMANDER_PATH:-}"
+
+    sudo nix flake update frontArmToPlane
+    sudo git add ./flake.lock
+    sudo git commit -m "updating lockfile's frontArmToPlane input" ./flake.lock
+
+    if [[ "${"1:-"}" == "--rebuild" ]]; then
+      sudo nixos-rebuild switch --flake .#wranHearst
+    fi
+
+
+
+    cd "$initial_path"
+  '';
+}
