529e6d95c56c3e5f2aaf20bbb1fe7968511aa527
nrs-status <sebaarsim@gmail.com>
2026-09-04 22:30:45 +0000

    fixing git pkg typo

--- templeArtemisEphesus/git/default.nix
+++ templeArtemisEphesus/git/default.nix
@@ -3,12 +3,14 @@
   localLib,
   ...
 }:
+let 
+  gitConfig = pkgs.writeTextFile "git-config" (import ./config.nix { inherit pkgs; });
+in
 localLib.mkWrapperScript {
   name = "git";
   pkgToWrap = pkgs.git;
-  src = ./.;
   preExecCommands = [
     "rm -f ~/.gitconfig"
-    "cp $src/gitconfig ~/.config/git/config"
+    "cp ${gitConfig} ~/.config/git/config"
   ];
 }
