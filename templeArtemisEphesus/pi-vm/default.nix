a465d1c941d3174ce3af3fa32fb089530bd111a1
nrs-status <sebaarsim@gmail.com>
2026-09-05 12:25:31 +0000

    pi-vm wip

--- templeArtemisEphesus/pi-vm/default.nix
+++ templeArtemisEphesus/pi-vm/default.nix
@@ -1,8 +1,33 @@
-{ localPkgs, pkgsLib, ... }:
+{ pkgs, localPkgs, pkgsLib, ... }:
 pkgsLib.nixosSystem {
   system = "x86_64-linux";
   modules = [
     {
+      networking.hostName = "pi-vm";
+      system.stateVersion = "26.11";
+
+      services.getty.autologinUser = "root";
+
+      virtualisation.graphics = false;
+
+      virtualisation = {
+        memorySize = 2048;
+        cores = 4;
+        diskSize = 8192;
+      };
+
+      virtualisation.sharedDirectories.pi-config = {
+        source = "/home/sieyes/.pi";
+        target = "/root/.pi";
+      };
+
+      environment.systemPackages = with pkgs; [
+        pi-coding-agent
+        git
+        vim
+        xxd
+      ];
+
       systemd.services.main = {
         wantedBy = [ "multi-user.target" ];
         after = [ "network-online.target" ];
