76fd3599c905aa21c74c852db45fda6bd0f5e7c0
nrs-status <sebaarsim@gmail.com>
2026-09-07 06:08:09 +0000

    testing out `atuin` terminal history manager

--- pyramidGiza/sieyes.nix
+++ pyramidGiza/sieyes.nix
@@ -27,6 +27,7 @@ pkgs.mkShell {
       gum #testing it out to make quick TUIs
       tree-sitter #testing out whether I can use it to extract substrings
       pet # testing out, snippet manager
+      atuin # testing out, terminal history manager
 
       #testing these for a workflow for querying the psql server
       visidata
