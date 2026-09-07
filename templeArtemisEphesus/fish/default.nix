374829149de061ca7f09d4e31e8937a6c7544cce
nrs-status <sebaarsim@gmail.com>
2026-09-07 04:16:47 +0000

    unecessary command removed

--- templeArtemisEphesus/fish/default.nix
+++ templeArtemisEphesus/fish/default.nix
@@ -50,7 +50,6 @@ in
     pkgToWrap = pkgs.fish;
     preExecCommands = [
       "rm -rf ~/.config/fish"
-      "mkdir -p ~/.config/fish"
       "cp -r ${fishConfig} ~/.config/fish/"
       "chmod -R u+w ~/.config/fish"
     ];
