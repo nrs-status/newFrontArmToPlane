f909742a9f794ff2fafacfb59ac1b51dbfcb23a7
nrs-status <sebaarsim@gmail.com>
2026-09-09 22:13:05 +0000

    Revert "fixed getExe warning"
    This reverts commit b204fb79b8ae875feda88c2b4d77d416b7523556.

--- pyramidGiza/sieyes.nix
+++ pyramidGiza/sieyes.nix
@@ -57,7 +57,7 @@ pkgs.mkShell {
   shellHook = ''
     export THATWATERCHARMANDER_PATH=$(cat /run/secrets/paths/wranHearst/thatWaterCharmander) #required for script that updates twc's fatp input
     export FRONTARMTOPLANE_PATH=$(cat /run/secrets/paths/wranHearst/frontArmToPlane)
-    exec ${pkgsLib.getExe' localPkgs.nushell "nu"}
+    exec ${pkgsLib.getExe localPkgs.nushell}
     echo "sieyes shell loaded"
   '';
 }
