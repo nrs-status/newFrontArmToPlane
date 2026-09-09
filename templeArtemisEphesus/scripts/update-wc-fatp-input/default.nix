0be940af7f0000a2ea93f6009aed1d8d4321095b
nrs-status <sebaarsim@gmail.com>
2026-09-09 11:05:53 +0000

    fixing typo

--- templeArtemisEphesus/scripts/update-wc-fatp-input/default.nix
+++
@@ -1,8 +0,0 @@
-{ localPkgs, pkgs, pkgsLib, ... }:
-	let 
-	script = pkgs.writeText "update-twc-fatp-input.nu" (builtins.readFile ./update-twc-fatp-input.nu);
-in pkgs.writeShellApplication {
-		name = "update-twc-fatp-input";
-		text = "${pkgsLib.getExe localPkgs.nushell} --config ~/.config/nushell/config.nu ${script}";
-	}
-
