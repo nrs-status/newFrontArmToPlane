4dae55f44eb42eaa955608f62f408add3d1f17c3
nrs-status <sebaarsim@gmail.com>
2026-09-10 00:04:41 +0000

    fix nxv flash config

--- templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/plugins/flash.nix
+++ templeArtemisEphesus/montezumaCirclesScroll/nixvimModules/plugins/flash.nix
@@ -6,9 +6,23 @@
     {
       key = "f"; # used to activate the hop plugin
       action.__raw = ''
-        function()
-          require'flash'.jump()
-        end
+                function()
+        require("flash").jump({
+          search = { forward = true, wrap = false, multi_window = false },
+        })
+                end
+      '';
+      options.remap = true;
+    }
+
+    {
+      key = "F"; # used to activate the hop plugin
+      action.__raw = ''
+                function()
+        require("flash").jump({
+          search = { forward = false, wrap = false, multi_window = false },
+        })
+                end
       '';
       options.remap = true;
     }
