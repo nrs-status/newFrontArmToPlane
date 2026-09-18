3c642f3ece3a3ab4f6f3768edc7531546b2b0983
nrs-status <sebaarsim@gmail.com>
2026-09-18 04:34:55 -0400

    Use peachRampSkateboard pkgsLib instead of pkgs.lib
    pkgsLib was previously just pkgs.lib, tying the glue code to the
    local pkgs instance. Source it from the peachRampSkateboard input
    so the repository's glue code matches the shared library it's
    built against.

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         system = "x86_64-linux";
         config.allowUnfree = true;
       };
-      pkgsLib = pkgs.lib; # pkgsLib is distinguished from pkgs because logically they are independent: pkgsLib is used to provide glue code to make the repository work, pkgs provides actual build components
+      pkgsLib = inputs.peachRampSkateboard.pkgsLib; # pkgsLib is distinguished from pkgs because logically they are independent: pkgsLib is used to provide glue code to make the repository work, pkgs provides actual build components
       baseLib = inputs.peachRampSkateboard.baseLib;
       localLib = import ./sandyFireworksBus {
         nixvimFlake = inputs.nixvimFlake;
