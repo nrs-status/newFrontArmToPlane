d379f786c966a7111909778f0a4df2513de34321
nrs-status <sebaarsim@gmail.com>
2026-09-22 07:37:08 -0400

    Squash commits from devshell-to-executable-script
    Combined commits:
    - make tmux derivation overridable with configurable default shell
    - add shellScriptOfDevshell.nix to turn mkShell into executable script
    - removing metadata

--- templeArtemisEphesus/tmux/default.nix
+++ templeArtemisEphesus/tmux/default.nix
@@ -1,25 +1,31 @@
 { pkgs, pkgsLib, ... }:
-pkgs.stdenv.mkDerivation {
-  name = "tmux";
-  src = ./.;
-  nativeBuildInputs = [ pkgs.makeWrapper ];
-  buildInputs = [ pkgs.tmux ];
-  installPhase = ''
-  runHook preInstall
+let
+  mkTmux =
+    { defaultShell }:
+    pkgs.stdenv.mkDerivation {
+      name = "tmux";
+      src = ./.;
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      buildInputs = [ pkgs.tmux ];
+      installPhase = ''
+        runHook preInstall
 
-  mkdir -p $out/bin $out/config
+        mkdir -p $out/bin $out/config
 
-  install -Dm644 inheritedConf.conf $out/config
-  install -Dm644 basic.conf $out/config
+        install -Dm644 inheritedConf.conf $out/config
+        install -Dm644 basic.conf $out/config
 
 
-  cat > $out/config/main.conf <<EOF
-    source-file $out/config/basic.conf
-    source-file $out/config/inheritedConf.conf
-  EOF
+        cat > $out/config/main.conf <<EOF
+          set -g default-shell ${defaultShell}
+          source-file $out/config/basic.conf
+          source-file $out/config/inheritedConf.conf
+        EOF
 
-  makeWrapper ${pkgsLib.getExe pkgs.tmux} $out/bin/tmux \
-  --add-flags "-f $out/config/main.conf"
-  
-  runHook postInstall'';
-}
+        makeWrapper ${pkgsLib.getExe pkgs.tmux} $out/bin/tmux \
+        --add-flags "-f $out/config/main.conf"
+
+        runHook postInstall'';
+    };
+in
+pkgsLib.makeOverridable mkTmux { defaultShell = pkgsLib.getExe pkgs.bash; }
